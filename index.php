0ae343b1100428c0a2933dd1e196d4ffd44c0c2b
dfox <dfox@foxartbox.com>
2023-11-06 19:35:28 +0200

    base. Part 3

--- index.php
+++ index.php
@@ -13,9 +13,30 @@
 </head>
 
 <body>
-<h1>Test 284</h1>
+<div class="wrapper">
+	<h1>Test 284</h1>
 
-<div>Something</div>
+	<form class="form_search js-form-search" action="/" method="post">
+		<input class="form_search__input" type="text" name="search" placeholder="Фильтр">
+
+		<label class="form_search__submit">
+			<input type="submit">
+			<span class="form_search__submit_text">OK</span>
+		</label>
+	</form>
+
+	<table class="data_table">
+		<thead>
+		<tr>
+			<td>Id</td>
+			<td>Name</td>
+			<td>Value</td>
+		</tr>
+		</thead>
+
+		<tbody class="js-data"></tbody>
+	</table>
+</div>
 
 <script src="js/main.js?v<?= (date("YmdHis")); ?>"></script>
 </body>
--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
 console.log('main.js loaded')
 
+const dataTable = document.querySelector('.js-data')
+
 const getData = () => {
 	fetch("js/data.json")
 		.then((res) => {
 			return res.json()
 		})
-		.then((data) => console.log(data))
+		// .then((data) => console.log(data))
+		.then((data) => {
+			console.log(data)
+
+			// id | name | value
+			data.forEach((e) => {
+				dataTable.innerHTML += '<tr><td>' + e.id + '</td><td>' + e.name + '</td><td>' + e.value + '</td></tr>'
+			})
+		})
 }
 
 getData()
